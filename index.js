d81ffa4a06200020095ecf5515ee53bcdaa5e882
Andrew Gnott <agnott@nd.edu>
2016-08-12 12:26:34 -0400

    Added db

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var bodyParser = require('body-parser');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// Begin server
+app.listen(process.env.PORT || 8080, function () {
+  console.log(`Example app listening on port ${process.env.PORT || 8080}!`);
+});
